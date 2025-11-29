c990ee5b6697a576bbd47d82a9744edd7c7af715
arda <48928106+ninthy@users.noreply.github.com>
2025-11-29 05:16:00 +0300

    Refactor patch.js for improved readability and structure
    Refactor patch.js to improve code structure and readability, including consistent use of quotes, error handling, and header management.

--- patch.js
+++ patch.js
@@ -1,204 +1,315 @@
-const { HttpsProxyAgent } = require('https-proxy-agent')
-const axios = require('axios').default
-const electron = require('electron')
-const express = require('express')
-const fs = require('fs')
+const axios = require("axios").default;
+const electron = require("electron");
+const express = require("express");
+const fs = require("fs");
 let gotScraping;
+const { decompress } = require("@mongodb-js/zstd");
 
-async function fetchWithGotScraping(url) {
-  gotScraping ??= (await import('got-scraping')).gotScraping;
+try {
+  async function fetchWithGotScraping(url) {
+    gotScraping ??= (await import("got-scraping")).gotScraping;
 
-  return gotScraping.get({
-    url: 'https://app.httptoolkit.tech' + url,
-    headerGeneratorOptions: {
-      browsers: [
-        {
-            name: 'chrome',
+    return gotScraping.get({
+      url: "https://app.httptoolkit.tech" + url,
+      headerGeneratorOptions: {
+        browsers: [
+          {
+            name: "chrome",
             minVersion: 87,
-            maxVersion: 89
-        }
-    ],
-    devices: ['desktop'],
-    locales: ['de-DE', 'en-US'],
-    operatingSystems: ['windows', 'linux'],
+            maxVersion: 89,
+          },
+        ],
+        devices: ["desktop"],
+        locales: ["de-DE", "en-US"],
+        operatingSystems: ["windows", "linux"],
+      },
+    });
+  }
+
+  function showPatchError(message) {
+    console.error(message);
+    electron.dialog.showErrorBox(
+      "Patch Error",
+      message +
+        "\n\nPlease report this issue on the GitHub repository (github.com/IPTVmanreal/httptoolkit-pro-patcher)"
+    );
+  }
+
+  const axiosInstance = axios.create({
+    baseURL: "https://app.httptoolkit.tech",
+    httpsAgent: undefined,
+  });
+
+  const hasInternet = () =>
+    axiosInstance
+      .head("/", {
+        headers: {
+          "Accept-Encoding": "gzip, deflate, br",
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) httptoolkit/1.19.1 Chrome/122.0.6261.130 Electron/29.1.5 Safari/537.36",
+        },
+      })
+      .then(() => true)
+      .catch(() => false);
+
+  const port = process.env.PORT || 5067;
+  const tempPath = path.join(os.tmpdir(), "httptoolkit-patch");
+
+  process.env.APP_URL = `http://localhost:${port}`;
+  console.log(`[Patcher] Selected temp path: ${tempPath}`);
+
+  const app = express();
+
+  app.disable("x-powered-by");
+
+  app.use(async (req, res) => {
+    console.log(`[Patcher] Request to: ${req.url}`);
+
+    const requestURL = new URL(req.url, process.env.APP_URL).pathname;
+
+    let filePath = path.join(
+      tempPath,
+      requestURL === "/" ? "index.html" : requestURL
+    );
+
+    if (["/view", "/intercept", "/settings", "/mock"].includes(requestURL)) {
+      filePath += ".html";
     }
-  })
-};
 
-function showPatchError(message) {
-  console.error(message)
-  electron.dialog.showErrorBox('Patch Error', message + '\n\nPlease report this issue on the GitHub repository (github.com/IPTVmanreal/httptoolkit-pro-patcher)')
-}
+    if (requestURL === "/ui-update-worker.js") {
+      console.log(`[Patcher] Preventing service worker from loading`);
+      res
+        .header("Content-Type", "application/javascript")
+        .send(
+          'self.addEventListener("install",e=>{e.waitUntil(self.skipWaiting()),console.log("[Patcher] HTTP Toolkit patched successfully =3")}),self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim())}),self.addEventListener("fetch",()=>{});'
+        );
+      return;
+    }
 
-const axiosInstance = axios.create({
-  baseURL: 'https://app.httptoolkit.tech',
-  httpsAgent:
-    globalProxy
-      ? new HttpsProxyAgent(
-          globalProxy.startsWith('http')
-            ? globalProxy.replace(/^http:/, 'https:')
-            : 'https://' + globalProxy
+    if (!fs.existsSync(tempPath)) {
+      console.log(`[Patcher] Temp path not found, creating: ${tempPath}`);
+      fs.mkdirSync(tempPath);
+    }
+
+    if (!(await hasInternet())) {
+      console.log(
+        `[Patcher] No internet connection, trying to serve directly from temp path`
+      );
+      if (fs.existsSync(filePath)) {
+        console.log(`[Patcher] Serving from temp path: ${filePath}`);
+        res.sendFile(filePath);
+      } else {
+        console.log(`[Patcher] File not found in temp path: ${filePath}`);
+        const error = `No internet connection and file is not cached for file: ${requestURL}`;
+        res
+          .status(200)
+          .send(
+            path.extname(filePath) === ".js"
+              ? `console.error(\`${error}\`);`
+              : error
+          );
+      }
+      return;
+    }
+
+    const reqHeaders = req.headers;
+    reqHeaders.host = "app.httptoolkit.tech";
+    reqHeaders.referer = "https://app.httptoolkit.tech/";
+    reqHeaders.origin = "https://app.httptoolkit.tech";
+
+    try {
+      if (fs.existsSync(filePath)) {
+        try {
+          const remoteDate = await axiosInstance
+            .head(req.url, { headers: reqHeaders })
+            .then((res) => new Date(res.headers["last-modified"]));
+          if (remoteDate < new Date(fs.statSync(filePath).mtime)) {
+            console.log(`[Patcher] File not changed, serving from temp path`);
+            res.sendFile(filePath);
+            return;
+          }
+        } catch (e) {
+          console.error(`[Patcher] [ERR] Failed to fetch remote file date`, e);
+        }
+      } else console.log(`[Patcher] File not found in temp path, downloading`);
+
+      const remoteFile = await axiosInstance.get(req.url, {
+        headers: reqHeaders,
+        responseType: "arraybuffer",
+      });
+
+      for (const [key, value] of Object.entries(remoteFile.headers)) {
+        if (key.toLowerCase() === "content-encoding") continue;
+        res.setHeader(key, value);
+      }
+
+      let dataBuf = Buffer.from(remoteFile.data);
+      const encoding = remoteFile.headers["content-encoding"];
+      console.log(`[Patcher] Encoding: ${encoding}`);
+
+      if (encoding === "zstd") {
+        try {
+          dataBuf = await decompress(dataBuf);
+        } catch (err) {
+          showPatchError(`[Patcher] [ERR] zstd decompression failed: ${err}`);
+        }
+        delete remoteFile.headers["content-encoding"];
+      }
+
+      if (requestURL === "/main.js") {
+        console.log(`[Patcher] Patching main.js`);
+        res.setHeader("Cache-Control", "no-store");
+
+        if (!dataBuf || dataBuf.length === 0) {
+          const remoteFile2 = await fetchWithGotScraping(req.url);
+          dataBuf = Buffer.from(remoteFile2.body);
+          console.log("remoteFile", remoteFile2.statusCode);
+        }
+
+        let data = dataBuf.toString("utf8");
+
+        const accStoreName = data.match(
+          /class ([0-9A-Za-z_$]+){constructor\(e\){this\.goToSettings=e/
+        )?.[1];
+
+        const modName = data.match(
+          /([0-9A-Za-z_$]+)\.(getLatestUserData|getLastUserData)/
+        )?.[1];
+
+        console.log(accStoreName, modName);
+
+        if (!accStoreName) {
+          showPatchError(
+            `[Patcher] [ERR] Account store name not found in main.js`
+          );
+        } else if (!modName) {
+          showPatchError(`[Patcher] [ERR] Module name not found in main.js`);
+        } else {
+          let patched = data.replace(
+            `class ${accStoreName}{`,
+            `["getLatestUserData","getLastUserData"].forEach(p=>Object.defineProperty(${modName},p,{value:()=>user}));class ${accStoreName}{`
+          );
+
+          if (patched === data) {
+            showPatchError(`[Patcher] [ERR] Patch failed`);
+          } else {
+            patched =
+              `const user=${JSON.stringify({
+                email: "pro@httptoolkit.com",
+                userId: "email|pro@httptoolkit.com",
+                subscription: {
+                  status: "active",
+                  quantity: 1,
+                  expiry: new Date("9999-12-31").toISOString(),
+                  sku: "pro-annual",
+                  plan: "pro-annual",
+                  tierCode: "pro",
+                  interval: "annual",
+                  canManageSubscription: true,
+                  updateBillingDetailsUrl:
+                    "https://github.com/IPTVmanreal/httptoolkit-pro-patcher",
+                },
+                teamSubscription: null,
+                featureFlags: [],
+                experiments: [],
+                flags: [],
+                features: [],
+                ruleParameters: [],
+                rules: [],
+                items: [],
+                version: undefined,
+                settings: {},
+                preferences: {},
+                banned: false,
+              })};user.subscription.expiry=new Date(user.subscription.expiry);\n` +
+              patched;
+
+            data = patched;
+            console.log(`[Patcher] main.js patched`);
+          }
+        }
+
+        const outBuf = Buffer.from(data, "utf8");
+        fs.writeFileSync(filePath, outBuf);
+        res.removeHeader("Content-Encoding");
+        res.setHeader("Content-Type", "application/javascript");
+        return res.status(200).send(outBuf);
+      }
+
+      if (filePath.endsWith(".js")) {
+        res.removeHeader("Content-Encoding");
+      }
+
+      if (dataBuf.length === 0) {
+        console.error(
+          `[Patcher] [ERR] Empty response for file: ${filePath}`,
+          remoteFile
+        );
+        const error = `Empty response for file: ${requestURL}`;
+        res
+          .status(200)
+          .send(
+            path.extname(filePath) === ".js"
+              ? `console.error(\`${error}\`);document.dispatchEvent(Object.assign(new Event('load:failed'),{error:new Error(\`${error}\`)}))`
+              : error
+          );
+        return;
+      } else {
+        fs.writeFileSync(filePath, dataBuf);
+      }
+
+      console.log(`[Patcher] File downloaded and saved: ${filePath}`);
+      res.sendFile(filePath);
+    } catch (e) {
+      console.error(
+        `[Patcher] [ERR] Failed to fetch remote file: ${filePath}`,
+        e
+      );
+      res.status(500).send("Internal server error");
+    }
+  });
+
+  app.listen(port, () =>
+    console.log(`[Patcher] Server listening on port ${port}`)
+  );
+
+  electron.app.on("ready", () => {
+    electron.app.on("browser-window-created", (event, window) => {
+      const title = "HTTPToolkit Patched";
+
+      window.setTitle(title);
+
+      window.webContents.on("page-title-updated", (e) => {
+        e.preventDefault();
+        window.setTitle(title);
+      });
+      // window.webContents.openDevTools({ mode: "detach" });
+    });
+
+    electron.session.defaultSession.webRequest.onBeforeSendHeaders(
+      (details, callback) => {
+        const blockedHosts = ["events.httptoolkit.tech"];
+        if (
+          blockedHosts.includes(new URL(details.url).hostname) ||
+          details.url.includes("sentry")
         )
-      : undefined //? Use proxy if set (globalProxy is injected by the patcher)
-})
-
-const hasInternet = () => axiosInstance.head('/', { headers: { 'Accept-Encoding': 'gzip, deflate, br', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) httptoolkit/1.19.1 Chrome/122.0.6261.130 Electron/29.1.5 Safari/537.36' } }).then(() => true).catch(() => false)
-
-const port = process.env.PORT || 5067
-const tempPath = path.join(os.tmpdir(), 'httptoolkit-patch')
-
-process.env.APP_URL = `http://localhost:${port}`
-console.log(`[Patcher] Selected temp path: ${tempPath}`)
-
-const app = express()
-
-app.disable('x-powered-by')
-
-app.all('*', async (req, res) => {
-  console.log(`[Patcher] Request to: ${req.url}`)
-
-  const requestURL = new URL(req.url, process.env.APP_URL).pathname
-
-  let filePath = path.join(tempPath, requestURL === '/' ? 'index.html' : requestURL)
-
-  if (['/view', '/intercept', '/settings', '/mock'].includes(requestURL)) {
-    filePath += '.html'
-  }
-
-  //? Prevent loading service worker to avoid caching issues
-  if (requestURL === '/ui-update-worker.js') {
-    console.log(`[Patcher] Preventing service worker from loading`)
-    res.header('Content-Type', 'application/javascript').send('self.addEventListener("install",e=>{e.waitUntil(self.skipWaiting()),console.log("[Patcher] HTTP Toolkit patched successfully =3")}),self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim())}),self.addEventListener("fetch",()=>{});')
-    return
-  }
-
-  if (!fs.existsSync(tempPath)) {
-    console.log(`[Patcher] Temp path not found, creating: ${tempPath}`)
-    fs.mkdirSync(tempPath)
-  }
-
-  if (!(await hasInternet())) {
-    console.log(`[Patcher] No internet connection, trying to serve directly from temp path`)
-    if (fs.existsSync(filePath)) {
-      console.log(`[Patcher] Serving from temp path: ${filePath}`)
-      res.sendFile(filePath)
-    } else {
-      console.log(`[Patcher] File not found in temp path: ${filePath}`)
-      const error = `No internet connection and file is not cached for file: ${requestURL}`
-      res.status(200).send(path.extname(filePath) === '.js' ? `console.error(\`${error}\`);` : error)
-    }
-    return
-  }
-
-  const reqHeaders = req.headers
-  reqHeaders.host = 'app.httptoolkit.tech'
-  reqHeaders.referer = 'https://app.httptoolkit.tech/'
-  reqHeaders.origin = 'https://app.httptoolkit.tech'
-
-  try {
-    if (fs.existsSync(filePath)) { //? Check if file exists in temp path
-      try {
-        const remoteDate = await axiosInstance.head(req.url, { headers: reqHeaders }).then(res => new Date(res.headers['last-modified']))
-        if (remoteDate < new Date(fs.statSync(filePath).mtime)) {
-          console.log(`[Patcher] File not changed, serving from temp path`)
-          res.sendFile(filePath)
-          return
-        }
-      } catch (e) {
-        console.error(`[Patcher] [ERR] Failed to fetch remote file date`, e)
+          return callback({ cancel: true });
+        details.requestHeaders.Origin = "https://app.httptoolkit.tech";
+        callback({ requestHeaders: details.requestHeaders });
       }
-    } else console.log(`[Patcher] File not found in temp path, downloading`)
-
-    const remoteFile = await axiosInstance.get(req.url, {
-      headers: reqHeaders,
-      responseType: 'arraybuffer'
-    })
-
-    for (const [key, value] of Object.entries(remoteFile.headers)) res.setHeader(key, value)
-
-    const recursiveMkdir = dir => {
-      if (!fs.existsSync(dir)) {
-        recursiveMkdir(path.dirname(dir))
-        fs.mkdirSync(dir)
+    );
+    electron.session.defaultSession.webRequest.onHeadersReceived(
+      (details, callback) => {
+        details.responseHeaders["Access-Control-Allow-Origin"] = [
+          `http://localhost:${port}`,
+        ];
+        delete details.responseHeaders["access-control-allow-origin"];
+        callback({ responseHeaders: details.responseHeaders });
       }
-    }
+    );
+  });
 
-    recursiveMkdir(path.dirname(filePath))
-
-    let data = remoteFile.data
-
-    if (requestURL === '/main.js') { //? Patch main.js
-      console.log(`[Patcher] Patching main.js`)
-      res.setHeader('Cache-Control', 'no-store') //? Prevent caching
-
-      data = data.toString()
-
-      if (data === '') {
-        const remoteFile = await fetchWithGotScraping(req.url);
-        data = remoteFile.body.toString()
-        console.log("remoteFile", remoteFile.statusCode)
-      }
-
-      const accStoreName = data.match(/class ([0-9A-Za-z_$]+){constructor\(e\){this\.goToSettings=e/)?.[1]
-      const modName = data.match(/([0-9A-Za-z_$]+).(getLatestUserData|getLastUserData)/)?.[1]
-
-      if (!accStoreName) showPatchError(`[Patcher] [ERR] Account store name not found in main.js`)
-      else if (!modName) showPatchError(`[Patcher] [ERR] Module name not found in main.js`)
-      else {
-        let patched = data
-          .replace(`class ${accStoreName}{`, `["getLatestUserData","getLastUserData"].forEach(p=>Object.defineProperty(${modName},p,{value:()=>user}));class ${accStoreName}{`)
-        if (patched === data) showPatchError(`[Patcher] [ERR] Patch failed`)
-        else {
-          patched = `const user=${JSON.stringify({
-            email, //? Injected by the patcher
-            subscription: {
-              status: 'active',
-              quanity: 1,
-              expiry: new Date('9999-12-31').toISOString(),
-              sku: 'pro-annual',
-              plan: 'pro-annual',
-              tierCode: 'pro',
-              interval: 'annual',
-              canManageSubscription: true,
-              updateBillingDetailsUrl: 'https://github.com/IPTVmanreal/httptoolkit-pro-patcher',
-            }
-          })};user.subscription.expiry=new Date(user.subscription.expiry);` + patched
-          data = patched
-          console.log(`[Patcher] main.js patched`)
-        }
-      }
-    }
-    if (data === '') {
-      console.error(`[Patcher] [ERR] Empty response for file: ${filePath}`, remoteFile)
-      const error = `Empty response for file: ${requestURL}`
-      res.status(200).send(path.extname(filePath) === '.js' ? `console.error(\`${error}\`);document.dispatchEvent(Object.assign(new Event('load:failed'),{error:new Error(\`${error}\`)}))` : error)
-      return
-    } else fs.writeFileSync(filePath, data)
-    console.log(`[Patcher] File downloaded and saved: ${filePath}`)
-    res.sendFile(filePath)
-  } catch (e) {
-    console.error(`[Patcher] [ERR] Failed to fetch remote file: ${filePath}`, e)
-    res.status(500).send('Internal server error')
-  }
-})
-
-app.listen(port, () => console.log(`[Patcher] Server listening on port ${port}`))
-
-electron.app.on('ready', () => {
-  //? Patching CORS headers to allow requests from localhost
-  electron.session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    //* Blocking unwanted requests to prevent tracking
-    const blockedHosts = ['events.httptoolkit.tech']
-    if (blockedHosts.includes(new URL(details.url).hostname) || details.url.includes('sentry')) return callback({ cancel: true })
-    details.requestHeaders.Origin = 'https://app.httptoolkit.tech'
-    callback({ requestHeaders: details.requestHeaders })
-  })
-  electron.session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-    details.responseHeaders['Access-Control-Allow-Origin'] = [`http://localhost:${port}`]
-    delete details.responseHeaders['access-control-allow-origin']
-    callback({ responseHeaders: details.responseHeaders })
-  })
-})
-
-//? Disable caching for all requests
-electron.app.commandLine.appendSwitch('disable-http-cache')
+  electron.app.commandLine.appendSwitch("disable-http-cache");
+} catch (e) {
+  console.error(`[Patcher] [ERR] Failed to patch HTTP Toolkit`, e);
+}
